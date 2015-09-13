1033ff73e1c39a529f0e0b09b49d8c7941498a85
Ilkka Rauta <ilkka.rauta@gmail.com>
2015-09-13 22:52:12 +0300

    Version that does something. (Clears the buffer.)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+
+extern crate glfw;
+extern crate regl;
+
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    let (mut window, events) = glfw.create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    regl::load_with(|s| window.get_proc_address(s));
+
+    let context = regl::Context::new();
+    let default_fb = context.default_framebuffer();
+    default_fb.clear_color(0.5, 0.5, 0.5, 1.0).unwrap();
+
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_event(&mut window, event);
+        }
+
+        default_fb.clear().unwrap();
+
+        window.swap_buffers();
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true)
+        }
+        _ => {}
+    }
 }
