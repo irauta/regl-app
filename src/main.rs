41f199d551fcdda2e1cb8f67153c7077dcdaf262
Ilkka Rauta <ilkka.rauta@gmail.com>
2015-09-22 22:16:04 +0300

    Render a triangle!

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,72 @@ extern crate glfw;
 extern crate regl;
 
 use glfw::{Action, Context, Key};
+use regl::{Shader,ShaderSource,ShaderType,Program,
+    VertexArray,Buffer,BufferTarget,BufferUsage,
+    VertexAttribute,VertexAttributeType,PrimitiveMode};
+
+#[allow(dead_code)]
+#[repr(C,packed)]
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[allow(dead_code)]
+#[repr(C,packed)]
+struct Rgba {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+#[allow(dead_code)]
+#[repr(C,packed)]
+struct Vertex {
+    position: Vec3,
+    color: Rgba
+}
+
+impl Vertex {
+    fn new(x: f32, y: f32, z: f32, r: u8, g: u8, b: u8, a: u8) -> Vertex {
+        Vertex { position: Vec3 { x: x, y: y, z: z }, color: Rgba { r: r, g: g, b: b, a: a } }
+    }
+}
+
+static VS_SOURCE: &'static str = "
+#version 330 core
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec4 color;
+
+out vec4 v_color;
+
+void main() {
+    gl_Position.xyz = position;
+    gl_Position.w = 1.0;
+    v_color = color;
+}
+";
+
+static FS_SOURCE: &'static str = "
+#version 330 core
+
+in vec4 v_color;
+out vec3 color;
+
+void main() {
+    color = v_color.rgb;
+}
+";
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
 
     let (mut window, events) = glfw.create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
@@ -15,9 +78,39 @@ fn main() {
 
     regl::load_with(|s| window.get_proc_address(s));
 
-    let context = regl::Context::new();
-    let default_fb = context.default_framebuffer();
-    default_fb.clear_color(0.5, 0.5, 0.5, 1.0).unwrap();
+    let mut context = regl::Context::new();
+    context.default_framebuffer().clear_color(0.0, 0.0, 0.0, 1.0);
+
+    let vertices = &[
+        Vertex::new(-0.5f32, -0.5f32, 0f32, 255, 0, 0, 0),
+        Vertex::new(0.5f32, -0.5f32, 0f32, 0, 255, 0, 0),
+        Vertex::new(0f32, 0.5f32, 0f32, 0, 0, 255, 0),
+    ];
+    let vbo = Buffer::new(&mut context, BufferTarget::VertexBuffer, BufferUsage::StaticDraw, vertices).unwrap();
+    let attributes = &[
+        VertexAttribute {
+            index: 0,
+            size: 3,
+            attribute_type: VertexAttributeType::Float,
+            normalized: false,
+            stride: 16,
+            offset: 0,
+            vertex_buffer: &vbo
+        },
+        VertexAttribute {
+            index: 1,
+            size: 4,
+            attribute_type: VertexAttributeType::UnsignedByte,
+            normalized: true,
+            stride: 16,
+            offset: 12,
+            vertex_buffer: &vbo
+        }
+    ];
+    let vao = VertexArray::new(&mut context, attributes, None).unwrap();
+    let vs = Shader::new(&mut context, &ShaderSource(ShaderType::VertexShader, VS_SOURCE)).unwrap();
+    let fs = Shader::new(&mut context, &ShaderSource(ShaderType::FragmentShader, FS_SOURCE)).unwrap();
+    let program = Program::new(&mut context, &[vs, fs]).unwrap();
 
     while !window.should_close() {
         glfw.poll_events();
@@ -25,7 +118,8 @@ fn main() {
             handle_window_event(&mut window, event);
         }
 
-        default_fb.clear().unwrap();
+        context.default_framebuffer().clear();
+        context.draw(&program, context.default_framebuffer(), &vao, PrimitiveMode::Triangles, 0, 3);
 
         window.swap_buffers();
     }
